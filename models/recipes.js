3dff72ca4395d228f879e5d0376ec5ada513e21f
Lockett Pundt <kalifornium@hotmail.com>
2020-03-09 16:58:14 -0400

    added an addRecip() and get comments() method.

--- models/recipes.js
+++ models/recipes.js
@@ -18,10 +18,39 @@ class Recipes {
             return error;
         }
     }
+
+    static async getReviewer(recipe_id) {
+        try {
+            const response = await db.any(`SELECT first_name, last_name FROM users JOIN recipes ON submit_id = users.id WHERE recipes.id = '${recipe_id}';`);
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
     
     static async getOne(id) {
         try {
             const response = await db.any(`SELECT * from recipes WHERE id = ${id};`)
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    static async addRecipe(name, rating, directions, ingredients, img, submit_id) {
+        try {
+            const response = await db.one(`INSERT INTO recipes 
+            (name, rating, directions, ingredients, img, submit_id) 
+            VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`, [name, rating, directions, ingredients, img, submit_id]);
+            return response;
+        } catch (error) {
+            return error;
+        }
+    }
+
+    static async getComments(recipe_id) {
+        try {
+            const response = await db.any(`SELECT * FROM comments WHERE recipes_id = ${recipe_id};`);
             return response;
         } catch (error) {
             return error;
