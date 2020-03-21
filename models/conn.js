f51db7d12723bbb85f1c288b74b47f7f94f5f7f5
Lockett Pundt <kalifornium@hotmail.com>
2020-03-21 19:36:42 -0400

    linted via airbnb

--- models/conn.js
+++ models/conn.js
@@ -1,17 +1,16 @@
-'use strict'
+
 require('dotenv').config();
 
 const options = {
-    host: process.env['DB_HOST'],
-    database: process.env['DB_NAME']
-}
-
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+};
 
 
 const pgp = require('pg-promise')({
-    query: e => {
+  query: (e) => {
     console.log('QUERY', e.query);
-    }
+  },
 });
 
 const db = pgp(options);
--- models/userModel.js
+++ models/userModel.js
@@ -1,95 +1,73 @@
-'use strict'
-const db = require('./conn');
+/* eslint-disable camelcase */
+
 const bcrypt = require('bcryptjs');
+const db = require('./conn');
 
 class UserModel {
-    constructor(id, first_name, last_name, email, password) {
-        this.id = id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
-        this.password = password;
-    }
+  constructor(id, first_name, last_name, email, password) {
+    this.id = id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.password = password;
+  }
 
-    checkPassword(hashedPassword) {
-        return bcrypt.compareSync(this.password, hashedPassword);
-    }
+  checkPassword(hashedPassword) {
+    return bcrypt.compareSync(this.password, hashedPassword);
+  }
 
-    async createUser() {
-        try {
-            const response = await db.one(`INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id`, [this.first_name, this.last_name, this.email, this.password]);
-            console.log('adding user: ', this.first_name, this.last_name);
-            return response;
-        } catch (error) {
-            return error;
-        }
+  async createUser() {
+    try {
+      const response = await db.one('INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING id', [this.first_name, this.last_name, this.email, this.password]);
+      return response;
+    } catch (error) {
+      return error;
     }
+  }
 
-    async existingEmail() {
-        try {
-            const response = await db.any(`SELECT * FROM users WHERE email = '${this.email}';`);
-            console.log(response);
-            return response.length ? true : false;
-        } catch (error) {
-            return error;
-        }
+  async existingEmail() {
+    try {
+      const response = await db.any(`SELECT * FROM users WHERE email = '${this.email}';`);
+      return !!response.length;
+    } catch (error) {
+      return error;
     }
+  }
 
-    async logIn() {
-        try {
-            const response = await db.one(`SELECT id, first_name, last_name, password FROM users WHERE email = $1`, [this.email]);
-            const {id, first_name, last_name, password} = response;
-            console.log('logging in user', first_name, last_name, this.email);
-            const isValid = this.checkPassword(password);
-            
-            return !!isValid ? {isValid, id, first_name, last_name} : isValid;
-        } catch (error) {
-            return error;
-        }
+  async logIn() {
+    try {
+      const response = await db.one('SELECT id, first_name, last_name, password FROM users WHERE email = $1', [this.email]);
+      const {
+        id, first_name, last_name, password,
+      } = response;
+      const isValid = this.checkPassword(password);
+
+      return isValid ? {
+        isValid, id, first_name, last_name,
+      } : isValid;
+    } catch (error) {
+      return error;
     }
+  }
 
-    static async leaveComment(title, comment, rating, users_id, recipes_id) {
-        try {
-            const response = await db.one(`INSERT INTO comments (title, comment, rating, users_id, recipes_id) 
+  static async leaveComment(title, comment, rating, users_id, recipes_id) {
+    try {
+      const response = await db.one(`INSERT INTO comments (title, comment, rating, users_id, recipes_id) 
             VAlUES ($1, $2, $3, $4, $5) RETURNING id`, [title, comment, rating, users_id, recipes_id]);
-            return response;
-        } catch (error) {
-            return error;
-        }
+      return response;
+    } catch (error) {
+      return error;
     }
-    static async getCommenter(id) {
-        try {
-            const response = await db.one(`SELECT * FROM users WHERE id = ${id};`);
-            return response;
-        } catch (error) {
-            return error;
-        }
+  }
+
+  static async getCommenter(id) {
+    try {
+      const response = await db.one(`SELECT * FROM users WHERE id = ${id};`);
+      return response;
+    } catch (error) {
+      return error;
     }
+  }
 }
 
 module.exports = UserModel;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = UserModel;
-
-
-
